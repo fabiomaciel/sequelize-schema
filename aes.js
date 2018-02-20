b8d853c68a67696b79b8cdc2b3dbb62506b590f3
Fabio Maciel <fabio.souza@metasix.com.br>
2018-02-20 11:56:55 -0300

    Realocated files to better structure

--- aes.js
+++
@@ -1,8 +0,0 @@
-const AES = require('aes');
-
-var key = '[itp.w~{aa~3p}Yg'.split('').map(e => e.charCodeAt())
-var aes = new AES(key);
-
-k = aes.encrypt('mensagem para criptografar');
-
-console.log(k)
